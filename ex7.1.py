2ccc0917385f053f7eee609b246cec602beaa5d6
JaronX <alexander.nikolov092@gmail.com>
2019-02-06 23:18:22 +0100

    Study Drills

---
+++ ex7.1.py
@@ -0,0 +1,21 @@
+print("Mary had a little lamb.") #print information inside the string
+print("Its fleece was white as {}.".format('snow')) #print information inside the string but replace {} with word - snow -
+print("And everywhere that Mary went.") # print information inside the string
+print("." * 10) # what'd that do # this mulptiplies 10 times information inside te string, so - . -
+
+end1 = "C" #define variable end1 as string with value "C"
+end2 = "h" #define variable end2 as string with value "h"
+end3 = "e" #define variable end3 as string with value "e"
+end4 = "e" #define variable end4 as string with value "e"
+end5 = "s" #define variable end5 as string with value "s"
+end6 = "e" #define variable end6 as string with value "e"
+end7 = "B" #define variable end7 as string with value "B"
+end8 = "u" #define variable end8 as string with value "u"
+end9 = "r" #define variable end9 as string with value "r"
+end10 = "g" #define variable end10 as string with value "g"
+end11 = "e" #define variable end11 as string with value "e"
+end12 = "r" #define variable end12 as string with value "r"
+
+# watch end = ' ' at the end. try removing it to see wat happens  
+print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ') #attach each one variable to makes word "Cheese" and the last variable makes free space also mount up next print sequence with variables
+print(end7 + end8 + end9 + end10 + end11 + end12) #attach each one variable
