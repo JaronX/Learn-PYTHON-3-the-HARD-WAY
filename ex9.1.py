08e5fc72dc9e0ae363943620498d8dad4a68124f
JaronX <alexander.nikolov092@gmail.com>
2019-02-07 23:20:58 +0100

    Study Drills

---
+++ ex9.1.py
@@ -0,0 +1,14 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun" #define variable as string with value "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" #define variable as string with value "Jan Feb etc." but each month is on separated row because of \n = that means new row
+
+print("Here are the days: ", days) #print text inside the string and after that add information inside the variable days
+print("Here are the months: ", months) #print text inside the string and after that add information inside the variable months.
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as mysch as we like.
+Even 4 lines if we want, or 5, or 6.
+""") #print information inside the string but separate each sentence into a separate line row. So there I don't have to write \n, I hit enter and magic happens, because of """.
